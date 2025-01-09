807733c58ecd0f251cf9299a68717ab59c6604e7
Diya Benny <132542534+Diyab2003@users.noreply.github.com>
2025-01-09 12:16:39 +0530

    Add files via upload

---
+++ fork.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<unistd.h>
+void main() {
+pid_t id;
+id =fork();
+ if(id==0){ 
+   printf("Child Process\n");
+   printf("Process ID is %d\n",getpid());
+   printf("Parent ID is %d\n",getppid());
+ }
+ else if(id>0){
+   wait(NULL);
+  printf("Parent process\n");
+  printf("Process ID is %d\n",getpid());
+  }
+ else
+   printf("Fork Failed\n");
+}
+ 
